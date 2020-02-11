994a83d73cd619256cfb4db5ef09a7b467f0ed50
unknown <basenau.michel@web.de>
2020-02-11 09:26:20 +0100

    DayOfTheYear - Funktionen

--- main.c
+++ main.c
@@ -2,72 +2,109 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool isSchaltjahr( int jahr)
+
+
+
+int is_leapyear(int year)
 {
-    if(jahr%4==0)
+    if(year<0 || year<1582)
     {
-        if(jahr%100==0)
+        return -1;
+    }
+
+    if(year%4==0)
+    {
+        if(year%100==0)
         {
-            if(jahr%400==0)
+            if(year%400==0)
             {
-                return true;
+                return 1;
             }
             else
             {
-                return false;
+                return 0;
             }
         }
         else
         {
-            return true;
+            return 1;
         }
 
     }
     else
     {
-        return false;
+        return 0;
+    }
+}
+
+int get_days_for_month(int month, int year)
+{
+    int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if (month > 0 && month < 13)
+    {
+        if (is_leapyear(year) == 1)
+        {
+            int days[1] = {29};
+
+        }
+        return days[month-1];
+    }
+    else
+    {
+        return -1;
     }
 }
 
 
+int exist_date(int day, int month, int year){
+    if (get_days_for_month(month,year)>0 && day <= get_days_for_month(month,year) && year > 2400 && year < 1582 )
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+
+int day_of_the_year(int day, int month, int year){
+    if (exist_date(day,month,year) == 1)
+    {
+        int i;
+        int anzahlTage = 0;
+
+        for(i=1; i<month; i++)
+        {
+            anzahlTage += get_days_for_month(i,year);
+        }
+    anzahlTage += day;
+    return anzahlTage;
+
+    }
+
+
+
+}
+
 
 int main()
 {
-    int jahr = 0;
-    int monat = 0;
-    int tag = 0;
-    int i = 0;
-    int anzahlTage = 0;
-    int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    bool daysChanged = false;
-    while(1)
-    {
+        int dayOfYear = 0;
+        int year = 0;
+        int month = 0;
+        int day = 0;
         printf("Eingabe Jahr :");
-        scanf("%i",&jahr);
+        scanf("%i",&year);
         printf("Eingabe Monat :");
-        scanf("%i",&monat);
+        scanf("%i",&month);
         printf("Eingabe Tag :");
-        scanf("%i",&tag);
+        scanf("%i",&day);
+        dayOfYear = day_of_the_year(day,month,year);
 
-        if (isSchaltjahr(jahr) && daysChanged == false)
-        {
-            daysChanged = true;
-            tage[1] +=1;
-        }
+        printf("Nr. des Tages:%i",dayOfYear);
 
-        if ( (monat<1 || monat>12)&& (tag < 0 || tag > tage[monat-1]))
-        {
-            printf("Monat nicht gueltig\n");
-            continue;
-        }
+        return 0;
 
-        break;
-    }
-    for(i=1; i<monat; i++)
-    {
-        anzahlTage += tage[i-1];
-    }
-    anzahlTage += tag;
-    printf("%i",anzahlTage);
-    return 0;
 }
