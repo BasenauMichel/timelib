c2d36eb424d93b9cfa64b11c06d2eb6776cb29c9
btbtbt btbtbt <basenau.michel@web.de>
2020-02-14 19:58:09 +0100

    Added Comments

--- main.c
+++ main.c
@@ -2,27 +2,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "timelib.h"
+//including new Library timelib.h, to be able to use time functions
 
-
-
-
+/*
+author : Michel Basenau
+After the user enters an date, this program returns the
+No. of the day, if the Date is correct.
+When the program returns 0, then the Date was incorrect.
+*/
 
 
 int main()
 {
+
+//Declaring and Initializing needed Variables
         int dayOfYear;
         int year = 0;
         int month = 0;
         int day = 0;
-        printf("Eingabe Jahr :");
+//Scanning Date, split into day, month and year
+        printf("Enter Year :");
         scanf("%i",&year);
-        printf("Eingabe Monat :");
+        printf("Enter Month :");
         scanf("%i",&month);
-        printf("Eingabe Tag :");
+        printf("Enter Day :");
         scanf("%i",&day);
-        dayOfYear = day_of_the_year(day,month,year);
+//Calling DayofYear-Function of the Library timelib.h, which returns the No. of the Day, calculated with the parameters day, month and year
+        dayOfYear = day_of_the_year(day, month, year);
 
-        printf("Nr. des Tages:%i",dayOfYear);
+        printf("No. of the Day :%i", dayOfYear);
 
         return 0;
 
--- timelib.c
+++ timelib.c
@@ -1,6 +1,9 @@
+//This functions parameter is a value of the type integer, which should be a year.
+//This function returns an integer. A -1 if the year is incorrect, a 0 if the year is not a leap year and a 1 if it is a leap year.
+
 int is_leapyear(int year)
 {
-    if(year<0 || year<1582)
+    if(year<1582 || year>2400)
     {
         return -1;
     }
@@ -29,56 +32,66 @@ int is_leapyear(int year)
     }
 }
 
+//This function has 2 parameter of the type integer and returns an integer.
+//This function checks the month and returns a -1, if the month is incorrect.
+//Then it declares an array, with 12 values standing for days of the months in chronological order.
+//After testing if the entered year is a leap year, by calling another function, it changes the February, if its a leap year.
+//This function returns the amount of days, of the entered month.
 int get_days_for_month(int month, int year)
 {
-    if (month < 1 && month > 12)
+    if (month < 1 || month > 12)
     {
         return -1;
     }
     else
     {
-        int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
         if (is_leapyear(year) == 1)
         {
-            int days[1] = {29};
-
+             int days[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+             return days[month-1];
         }
-        return days[month-1];
+        else
+        {
+            int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+            return days[month-1];
+        }
     }
 }
 
-
+//This function has 3 parameters of the type integer, which form a date and returns an integer.
+//When the entered date does not exist, the function returns a 0. Else it returns a 1.
 int exist_date(int day, int month, int year)
 {
-    if (day > 0 && day <= get_days_for_month(month,year) && year < 2400 && year > 1582 )
-    {
-        return 1;
-    }
-    else
+    if (day <= 0 || day > get_days_for_month(month,year) || year > 2400 || year < 1582 )
     {
         return 0;
     }
+    else
+    {
+        return 1;
+    }
 }
 
 
-
+//This function has 3 parameters of the type integer, which form a date and returns a variable of the type integer.
+//The Variable returned by this function, contains the total amount of days, that have passed since the entered date
 int day_of_the_year(int day, int month, int year)
 {
     if (exist_date(day,month,year) == 1)
     {
         int i;
-        int anzahlTage = 0;
+        int TotalAmountDays = 0;
 
         for(i=1; i<month; i++)
         {
-            anzahlTage += get_days_for_month(i,year);
+            TotalAmountDays += get_days_for_month(i,year);
         }
-        anzahlTage += day;
-        return anzahlTage;
+        TotalAmountDays += day;
+        return TotalAmountDays;
 
     }
 }
+
 
 
 
